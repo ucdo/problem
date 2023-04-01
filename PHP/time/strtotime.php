dc7db205fc81f5712331aacfa645b2aa23ffd827
wangfuduo <1459325004@qq.com>
2023-04-01 11:00:46 +0800

    strtotime: -1 month因为大小月和PHP底层的机制，导致月份会有错误。使用修正短语 last day of - {$i} month 修复

---
+++ PHP/time/strtotime.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 令人困惑的 strtotime
+ */
+
+var_dump(date("Y-m-d", strtotime("-1 month", strtotime("2017-03-31"))));
+//输出2017-03-03
+var_dump(date("Y-m-d", strtotime("+1 month", strtotime("2017-08-31"))));
+//输出2017-10-01
+var_dump(date("Y-m-d", strtotime("next month", strtotime("2017-01-31"))));
+//输出2017-03-03
+var_dump(date("Y-m-d", strtotime("last month", strtotime("2017-03-31"))));
+
+var_dump(date("Y-m-d", strtotime("last day of -1 month", strtotime("2017-03-31"))));
+//输出2017-02-28
+var_dump(date("Y-m-d", strtotime("first day of +1 month", strtotime("2017-08-31"))));
+////输出2017-09-01
+var_dump(date("Y-m-d", strtotime("first day of next month", strtotime("2017-01-31"))));
+////输出2017-02-01
+var_dump(date("Y-m-d", strtotime("last day of last month", strtotime("2017-03-31"))));
+////输出2017-02-28
